7a1678cf518b319acd23160e8b14845dbeea3d75
Med-NAIMI <56475732+Med-NAIMI@users.noreply.github.com>
2021-04-25 18:55:19 +0200

    Add files via upload

---
+++ changerContenuLivre/changerContenuLivre/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changerContenuLivre
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("svp, entrez le nbr de personne qui stockent le livre!");
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            string continu = "oui";
+            string s1;
+            List<string> l = new List<string>();
+
+            while(continu=="oui")
+            {
+                Console.WriteLine("svp, entrez la proposition de modification accomagnée du nbr d'accord associé!");
+                Console.WriteLine("example de proposition (a 4) pour une proposition d'ecriture de a à 4 accords");
+
+                s1 = Console.ReadLine();
+
+                l.Add(s1);//ajout de proposition
+                Console.WriteLine("svp, entrez oui pour continuer la saisie de propositions!");
+                continu = Console.ReadLine();
+            }
+
+            Console.WriteLine("le livre resultant après modification est: "+Traitement.contenuLivre(l, p));
+            
+            Console.WriteLine("end");
+            
+            Console.ReadKey();//pour conserver l'affichage
+         }
+
+    
+    }
+}
---
+++ changerContenuLivre/changerContenuLivre/Traitement.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changerContenuLivre
+{
+    abstract class Traitement
+    {
+        public static String contenuLivre(List<string> l, int p )
+        {
+            Dictionary<char, int> tauxValidationChar = charTauxAffectation(l);
+            //extraction de taux de validation pour chaque char
+
+            int tauxAdmissible = p/2;//calcule du taux admissible
+
+            string s1= "";//initialisation de la chaine qui contiendra le livre de retour
+
+            //selection des char ayant un nbr de validation superieure a la moitié d'effectif
+            var admissibleChars = tauxValidationChar.Where(p1 => p1.Value>tauxAdmissible).Select(p1=>p1.Key).ToList();
+            foreach(char s2 in admissibleChars)
+            {
+                s1+=s2;//concaténation de tous les chars qui satisfait les conditions
+            }
+            //Console.WriteLine(s1);
+            return s1;
+        }
+
+        static Dictionary<char, int> charTauxAffectation(List<string> l)
+        {
+            Dictionary<char, int> tauxAffectationChar = new Dictionary<char, int>();
+            foreach(string s in l)
+            {
+                try
+                {
+                    
+                        if(!tauxAffectationChar.ContainsKey(s[0]))
+                    tauxAffectationChar.Add(s[0],(int)Char.GetNumericValue(s[2]));
+                    //nous nous intereserons a s[0] "le char" et s[2] "nbr de personnes qui valide ce char" 
+
+                    else//si la demande a propos de ce char est déja faite, nous enregistrons celle du plus grand nbr de validation
+                    {
+                        if (s[2] > tauxAffectationChar[s[0]])//
+                        {
+                            tauxAffectationChar.Remove(s[0]);
+                            tauxAffectationChar.Add(s[0], (int)Char.GetNumericValue(s[2]));
+                        }
+                        else
+                            continue;
+                        
+
+                    }
+
+                        
+                }
+                catch (IndexOutOfRangeException e)  
+                {
+                    throw new ArgumentOutOfRangeException("index parameter is out of range.", e);
+                }
+                               
+            }
+            return tauxAffectationChar;
+        }
+    }
+}
